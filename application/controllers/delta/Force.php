a2f9b10e7cced5a7189877038d02fba836f79fb6
kboranbayev <boranbayev.k@gmail.com>
2017-09-29 16:08:14 +0600

    added ctor and comments
    Signed-off-by: kboranbayev <boranbayev.k@gmail.com>

--- application/controllers/delta/Force.php
+++ application/controllers/delta/Force.php
@@ -1,6 +1,23 @@
 <?php
+
+/**
+ * Delta Force controller
+ * Shows quote with index 4 at /delta/force
+ */
 class Force extends Application
 {
+
+  /**
+   * Ctor
+   */
+  function __construct()
+  {
+    parent::__construct();
+  }
+
+  /**
+   *  index function 
+   */
   public function index() {
     $this->show(4);
   }
