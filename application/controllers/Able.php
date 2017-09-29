a7eb618cc84a283916b8469f5f351161d1a26d84
Spencer Leslie <spencerdleslie@gmail.com>
2017-09-29 12:26:12 -0700

    added comments

--- application/controllers/Able.php
+++ application/controllers/Able.php
@@ -1,9 +1,15 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+/**
+ * Able controller
+ * Shows the quote with index 1 at /able
+ */
 class Able extends Application
 {
-
+    /**
+     * Ctor
+     */
 	function __construct()
 	{
 		parent::__construct();
