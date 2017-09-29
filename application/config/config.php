b6c60d7a099b41a45c2fc9d91a838f2990f6dec7
kboranbayev <boranbayev.k@gmail.com>
2017-09-29 14:15:35 +0600

    Syncing with team repo
    Signed-off-by: kboranbayev <boranbayev.k@gmail.com>

--- application/config/config.php
+++ application/config/config.php
@@ -4,27 +4,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 // the menu basics (text navbar)
 $config['menu_choices'] = array(
     'menudata' => array(
-<<<<<<< HEAD
-        array('name' => 'Alpha', 'link' => '#'),
+        array('name' => 'Alpha', 'link' => '/able'),
         array('name' => 'Bravo', 'link' => '/bravo'),
-        array('name' => 'Charlie', 'link' => '#'),
+        array('name' => 'Charlie', 'link' => '/charlie/brown'),
         array('name' => 'Delta', 'link' => '/delta/force'),
         array('name' => 'Echo', 'link' => '/echo/must/wehave'),
         array('name' => 'Foxtrot', 'link' => '/foxtrot'),
         array('name' => 'Golf', 'link' => '/i/need/a/hobby'),
         array('name' => 'Hotel', 'link' => '/bananas/rule'),
         array('name' => 'India', 'link' => '/india'),
-=======
-        array('name' => 'Alpha', 'link' => '/able'),
-        array('name' => 'Bravo', 'link' => '#'),
-        array('name' => 'Charlie', 'link' => '/charlie/brown'),
-        array('name' => 'Delta', 'link' => '#'),
-        array('name' => 'Echo', 'link' => '#'),
-        array('name' => 'Foxtrot', 'link' => '#'),
-        array('name' => 'Golf', 'link' => '#'),
-        array('name' => 'Hotel', 'link' => '#'),
-        array('name' => 'India', 'link' => '#'),
->>>>>>> c1adfc4cfe7268bcdb1ed4c0ae1f363973562398
         array('name' => 'Juliet', 'link' => '#'),
         array('name' => 'Kilo', 'link' => '#'),
         )
