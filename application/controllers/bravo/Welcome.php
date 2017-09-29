18cea5afabb2cd0187e20dea45b1956f979052cc
kboranbayev <boranbayev.k@gmail.com>
2017-09-29 16:44:56 +0600

    Closes #2 - updated
    Signed-off-by: kboranbayev <boranbayev.k@gmail.com>

--- application/controllers/bravo/Welcome.php
+++ application/controllers/bravo/Welcome.php
@@ -1,9 +1,23 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
 /**
- *  Bravo controller 
+ *  Bravo controller
  */
 class Welcome extends Application
 {
+
+  /**
+   * Ctor
+   */
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+  /**
+   *  Welcome index 
+   */
   public function index() {
     // passes ''$key = 2' to show method in MY_Controller.php
     $this->show(2);
