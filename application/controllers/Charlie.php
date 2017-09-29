41126774876ef1fd686c608b6e9d5b89f9f80d8e
Spencer Leslie <spencerdleslie@gmail.com>
2017-09-29 12:27:31 -0700

    added comments

--- application/controllers/Charlie.php
+++ application/controllers/Charlie.php
@@ -1,9 +1,16 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+/**
+ * Charlie Brown controller
+ * Shows quote with index 3 at /charlie/brown
+ */
 class Charlie extends Application
 {
 
+    /**
+     * Ctor
+     */
 	function __construct()
 	{
 		parent::__construct();
